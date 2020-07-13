23f5ac806045c2384a54f614b6225a7d5b7ccb59
zakaria touzri <zakadmaster@gmail.com>
2020-07-13 20:00:39 +0200

    ADDED: get_value

--- singly_linked_list/singly_linked_list.py
+++ singly_linked_list/singly_linked_list.py
@@ -2,8 +2,10 @@ class Node:
     def __init__(self, value, next):
         self.value = value
         self.next = next
-    
+
+    def get_value(self):
+        return self.value
      
 
 class LinkedList:
-    def __init__(self)
+    def __init__(self):
