a51f7b4704ddc0ac10e4a6940f85c19639770451
zakaria touzri <zakadmaster@gmail.com>
2020-07-15 20:22:18 +0200

    ADDED: insert

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -17,7 +17,16 @@ class BSTNode:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        if value > self.value:
+            if self.right != None:
+                self.right.insert(value)
+                return
+            self.right = BSTNode(value)
+        else:
+            if self.left != None:
+                self.left.insert(value)
+                return
+            self.left = BSTNode(value)
 
     # Return True if the tree contains the value
     # False if it does not
@@ -59,3 +68,11 @@ class BSTNode:
     # Print Post-order recursive DFT
     def post_order_dft(self, node):
         pass
+
+bst = BSTNode(5)
+bst.insert(7)
+bst.insert(6)
+bst.insert(2)
+bst.insert(3)
+
+print("fin")
